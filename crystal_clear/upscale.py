5481d73a71aa27066f9e36713993029f9685eed0
Ludovic <ludovic.v.schwartz76@gmail.com>
2019-04-14 21:57:08 +0200

    Added a new upscaling method that considers the average of all overlapping windows containing the frame instead of distinct window.

--- crystal_clear/upscale.py
+++ crystal_clear/upscale.py
@@ -44,6 +44,86 @@ def upscale(path_file, learner=None):
     return song
 
 
+def make_weighted_average_on_window(array, predict_method, padded=True):
+    nc, sz, n = array.shape
+    if not padded:
+        array = np.pad(array, ((0, 0), (0, 0), (sz-1, sz-1)), mode='edge')
+    else:
+        n = n - 2 * (sz-1)
+    sliding = np.stack([predict_method(array[:, :, i:i+sz])
+                        for i in tqdm(range(n+sz-1))])
+
+    sliding_frame = np.lib.stride_tricks.as_strided(
+        # Reverse last dimension since we want the last column from the first
+        # frame.
+        # Need to copy again because `[::-1]` creates a view with negative
+        # stride, but we want actual reversal to work with the strides below.
+        # (There's perhaps a smart way of adjusting the strides below in order
+        # to not make a copy here.)
+        sliding[:, :, :, ::-1].copy(),
+        # Second dimension corresponds to the 128 consecutive frames.
+        # Previous last dimension is dropped since we're selecting the
+        # column that corresponds to the current n-position
+        shape=(sz, n, nc, sz),
+        # First dimension (frame position): Move one frame and one column ahead
+        # (actually want to move one column less in `sliding` but since we
+        # reverted order of columns we need to move one ahead now)
+        # -> move across first dimension of `sliding` + last dimension of
+        # `sliding`.
+        # Second dimension (n-position): Moving one frame ahead
+        # -> move across first dimension of `sliding`.
+        # Remaining two dimensions:
+        # Move within frames (channel and row dimensions).
+        strides=((sliding.strides[0] + sliding.strides[-1],)
+                 + sliding.strides[:1] + sliding.strides[1:3])
+    )
+    # Weights are independent of the n-position -> we can precompute.
+    parity = [] if sz % 2 == 0 else [sz//2]
+    weights = 1 + np.concatenate([np.arange(sz//2), parity,
+                                  np.arange(sz//2)[::-1]])
+    weights = weights.astype(float) / weights.sum()
+    weights = weights[:, None, None, None]  # Prepare for broadcasting.
+    return np.moveaxis(np.sum(sliding_frame * weights, axis=0), 0, 2)
+
+
+def upscale2(path_file, learner):
+    '''
+    Upscale the file located in path_file using the learner.
+    '''
+    nperseg = 254
+    sz = nperseg // 2 + 1
+    song = AudioSegment.from_file(path_file)
+    if learner is None:
+        return song
+    from_img = False
+    n_channels = song.channels
+    samples = np.array(song.get_array_of_samples()).reshape(n_channels,
+                                                            -1, order='F')
+    # return samples
+    sample_rate = song.frame_rate
+    # We 0-pad the edge of the music
+    samples = np.pad(samples, ((0, 0), (sz-1, sz-1)), mode='edge')
+    f, t, Zxx = scipy.signal.stft(samples, sample_rate, nperseg=nperseg)
+    t = t[sz-1:-sz+1]
+    data = make_into_3D(np.abs(Zxx))
+    Zxx = Zxx[:, :, sz-1:-sz+1]
+    n = data.shape[-1]
+
+    def predict_method(a):
+        m = a.max()
+        return learner.predict(a / m)[0].data.numpy().astype(np.float32) * m
+    data = make_weighted_average_on_window(data, predict_method)
+    # Same as upscale here
+    data = data[:n_channels]
+    angle = (Zxx / np.abs(Zxx))
+    angle[np.isnan(angle)] = 1
+    new_Zxx = angle * data
+    _, reconstructed = scipy.signal.istft(new_Zxx, sample_rate)
+    reconstructed = np.rint(reconstructed).astype(np.int16)
+    song._data = bytearray(reconstructed.reshape(1, -1, order='F'))
+    return song
+
+
 class FeatureLoss(nn.Module):
     def __init__(self, m_feat, layer_ids, layer_wgts):
         super().__init__()
@@ -70,4 +150,3 @@ class FeatureLoss(nn.Module):
         return sum(self.feat_losses)
 
     def __del__(self): self.hooks.remove()
-
